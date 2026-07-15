6c01e642c0cbf4d69b4e497c850a1e757de0ce83
Varun <ll.varun.ll@gmail.com>
2026-07-15 23:35:59 +0530

    cooked dosa stacking

--- main.gd
+++ main.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
+@export var cooked_dosa_scene: PackedScene
+
 var flag_stove_on = 0
 var flag_maav_picked = 0
 var flag_maav_veerti = 0
 var flag_dosa_cooked = 0
 var flag_dosa_flipped = 0
+
 var count_dosa = 0
+
 
 func ladle_picked_up():
 	$hand.ladle_picked()
@@ -29,13 +33,22 @@ func stove_pressed():
 		$dosa.maav_flipped()
 		flag_dosa_flipped = 1
 	elif flag_dosa_flipped == 1:
-		$cooked_dosa.show()
+		#$cooked_dosa.show()
 		$dosa.hide()
 		flag_maav_veerti = 0
 		flag_dosa_cooked = 0
 		flag_dosa_flipped = 0
 		count_dosa += 1
 		$hud/dosa_counter.text = str(count_dosa)
+		
+		var ckd_dos = cooked_dosa_scene.instantiate()
+		
+		var ckd_dos_spawn_location = $cooked_dosa_loc.position
+		ckd_dos_spawn_location.x += -10 + 20*randf()
+		ckd_dos_spawn_location.y -= 5 * (count_dosa-1)
+		ckd_dos.position = ckd_dos_spawn_location
+		add_child(ckd_dos)
+		
 		print('Dosa cooked = ')
 		print(count_dosa)
 
